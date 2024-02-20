322e8984edd4c014be26efd8075d4401c7b892b1
Vertragana <136757182+Vertragana@users.noreply.github.com>
2024-02-20 10:12:50 +0300

    with useQuery

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,31 +1,8 @@
-import {useState, useEffect} from "react";
-
-const localCache = {};
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
 export default function useBreedList(animal){
-    const [breedList, setBreedList] = useState([]);
-    const [status, setStatus] = useState("unloaded");
+   const results = useQuery(["breeds", animal], fetchBreedList);
 
-    useEffect(() => {
-        if (!animal){
-            setBreedList([]);                   //если не выбрано животное то пустой массив
-        } else if (localCache[animal]){
-            setBreedList(localCache[animal])    // если породы животных были в локалкэш то берем их из него
-        } else {
-            requestBreedList();                 // если первый раз выбираем тип животного то загружаем список пород
-        }
-
-        async function requestBreedList() {
-            setBreedList([]);
-            setStatus("loading");
-
-            const res = await fetch( `http://pets-v2.dev-apis.com/breeds?animal=${animal}`)
-            const json = await res.json();
-            localCache[animal]= json.breeds || [];
-            setBreedList(localCache[animal]);
-            setStatus("loaded");
-        }
-    }, [animal]);   // каждый раз когда меняется животное мы запрашиваем новый breedList 
-
-    return [breedList, status];
+    return [results?.data?.breeds ?? [], results.status];
 }
